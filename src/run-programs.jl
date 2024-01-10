03593b93aa6ebee7ac476cd7bc888f2923997ccb
J S <49557684+svilupp@users.noreply.github.com>
2024-01-10 21:47:30 +0000

    update kwargs and docs

--- src/run-programs.jl
+++ src/run-programs.jl
@@ -1,9 +1,11 @@
 # executables
 
 """
-    run_llama(; model::AbstractString, prompt::AbstractString="", nthreads::Int=1, n_gpu_layers::Int=99, ctx_size::Int=512, args=``)
+    run_llama(; model::AbstractString, prompt::AbstractString="", nthreads::Int=1, n_gpu_layers::Int=99, ctx_size::Int=2048, args=``)
 
 Runs `prompt` through the `model` provided and returns the result. This is a single-turn version of `run_chat`.
+
+See the [full documentation](https://github.com/ggerganov/llama.cpp/blob/master/examples/main/README.md) for more details.
 
 # Arguments
 - `model`: path to the model to be used
@@ -11,14 +13,14 @@ Runs `prompt` through the `model` provided and returns the result. This is a sin
 - `nthreads`: number of threads to use. Defaults to the number of available threads
 - `n_gpu_layers`: number of layers to offload on the GPU (a.k.a. `ngl` in llama.cpp). Requires more VRAM on your GPU but can speed up inference.
   Set to 0 to run inference on CPU-only. Defaults to 99 (=practically all layers)
-- `ctx_size`: context size, ie, how big can the prompt/inference be. Defaults to 512 (but most models allow 4,000 and more)
+- `ctx_size`: context size, ie, how big can the prompt/inference be. Defaults to 2048 (but most models allow 4,000 and more)
 
 Note: If you get odd responses AND you're using an instruction-tuned ("fine-tuned"), it might be that the format of your prompt is not correct. 
 See HuggingFace's model documentation for the correct prompt format or use a library that will do this for you (eg, PromptingTools.jl)
 
 See also: `run_chat`, `run_server`
 """
-function run_llama(; model::AbstractString, prompt::AbstractString="", nthreads::Int=Threads.nthreads(), n_gpu_layers::Int=99, ctx_size::Int=512, args=``)
+function run_llama(; model::AbstractString, prompt::AbstractString="", nthreads::Int=Threads.nthreads(), n_gpu_layers::Int=99, ctx_size::Int=2048, args=``)
     cmd = `$(llama_cpp_jll.main()) --model $model --prompt $prompt --threads $nthreads --n-gpu-layers $n_gpu_layers --ctx-size $ctx_size $args`
     # Provides the path to locate ggml-metal.metal file (must be provided separately)
     cmd = addenv(cmd, "GGML_METAL_PATH_RESOURCES" => joinpath(llama_cpp_jll.artifact_dir, "bin"))
@@ -26,14 +28,16 @@ function run_llama(; model::AbstractString, prompt::AbstractString="", nthreads:
 end
 
 """
-    run_chat(; model::AbstractString, prompt::AbstractString="", nthreads::Int=Threads.nthreads(), n_gpu_layers::Int=99, ctx_size::Int=512, args=``)
+    run_chat(; model::AbstractString, prompt::AbstractString="", nthreads::Int=Threads.nthreads(), n_gpu_layers::Int=99, ctx_size::Int=2048, args=``)
 
-Opens an interactive console for the `model`. 
+Opens an interactive console for the `model` and runs in "instruction" mode (especially useful for Alpaca-based models). 
 `prompt`, as the first message, is often used to provide instruction about the upcoming interactions (eg, style, tone, roles).
 
 Wait for model to reply and then type your response. Press `Enter` to send the message to the model.
 
-Interrupt the console with `Ctrl+C`
+Interrupt the chat with `Ctrl+C`
+
+See the [full documentation](https://github.com/ggerganov/llama.cpp/blob/master/examples/main/README.md) for more details.
 
 # Arguments
 - `model`: path to the model to be used
@@ -41,14 +45,14 @@ Interrupt the console with `Ctrl+C`
 - `nthreads`: number of threads to use. Defaults to the number of available threads
 - `n_gpu_layers`: number of layers to offload on the GPU (a.k.a. `ngl` in llama.cpp). Requires more VRAM on your GPU but can speed up inference.
   Set to 0 to run inference on CPU-only. Defaults to 99 (=practically all layers)
-- `ctx_size`: context size, ie, how big can the prompt/inference be. Defaults to 512 (but most models allow 4,000 and more)
+- `ctx_size`: context size, ie, how big can the prompt/inference be. Defaults to 2048 (but most models allow 4,000 and more)
 
 Note: If you get odd responses AND you're using an instruction-tuned ("fine-tuned"), it might be that the format of your prompt is not correct. 
 See HuggingFace's model documentation for the correct prompt format or use a library that will do this for you (eg, PromptingTools.jl)
 
-See also: `run_chat`, `run_server`
+See also: `run_llama`, `run_server`
 """
-function run_chat(; model::AbstractString, prompt::AbstractString="", nthreads::Int=Threads.nthreads(), n_gpu_layers::Int=99, ctx_size::Int=512, args=``)
+function run_chat(; model::AbstractString, prompt::AbstractString="", nthreads::Int=Threads.nthreads(), n_gpu_layers::Int=99, ctx_size::Int=2048, args=``)
     cmd = `$(llama_cpp_jll.main()) --model $model --prompt $prompt --threads $nthreads --n-gpu-layers $n_gpu_layers --ctx-size $ctx_size $args -ins`
     # Provides the path to locate ggml-metal.metal file (must be provided separately)
     cmd = addenv(cmd, "GGML_METAL_PATH_RESOURCES" => joinpath(llama_cpp_jll.artifact_dir, "bin"))
@@ -72,7 +76,7 @@ Interrupt the server with `Ctrl+C`.
 - `nthreads`: number of threads to use. Defaults to the number of available threads
 - `n_gpu_layers`: number of layers to offload on the GPU (a.k.a. `ngl` in llama.cpp). Requires more VRAM on your GPU but can speed up inference.
   Set to 0 to run inference on CPU-only. Defaults to 99 (=practically all layers)
-- `ctx_size`: context size, ie, how big can the prompt/inference be. Defaults to 512 (but most models allow 4,000 and more)
+- `ctx_size`: context size, ie, how big can the prompt/inference be. Defaults to 2048 (but most models allow 4,000 and more)
 
 See the [full documentation](https://github.com/ggerganov/llama.cpp/blob/master/examples/server/README.md) for more details.
 
@@ -92,7 +96,7 @@ Downloads.download("https://huggingface.co/TheBloke/dolphin-2_6-phi-2-GGUF/resol
 # Start the server
 run_server(; model)
 """
-function run_server(; model::AbstractString, host::AbstractString="127.0.0.1", port::Int=519, nthreads::Int=Threads.nthreads(), n_gpu_layers::Int=99, ctx_size::Int=512, args=``)
+function run_server(; model::AbstractString, host::AbstractString="127.0.0.1", port::Int=519, nthreads::Int=Threads.nthreads(), n_gpu_layers::Int=99, ctx_size::Int=2048, args=``)
     cmd = `$(llama_cpp_jll.server()) --model $model --host $host --port $port --threads $nthreads --n-gpu-layers $n_gpu_layers --ctx-size $ctx_size $args`
     # Provides the path to locate ggml-metal.metal file (must be provided separately)
     cmd = addenv(cmd, "GGML_METAL_PATH_RESOURCES" => joinpath(llama_cpp_jll.artifact_dir, "bin"))
