fd26e459661615bccd7bfc0ca3aba5d1ccb11c52
J S <49557684+svilupp@users.noreply.github.com>
2025-02-21 18:21:49 +0000

    update pkg UUID with the name change

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using LlamaCpp
+using Documenter
+
+DocMeta.setdocmeta!(LlamaCpp, :DocTestSetup, :(using LlamaCpp); recursive=true)
+
+makedocs(;
+    modules=[LlamaCpp],
+    authors="Marco Matthies <71844+marcom@users.noreply.github.com>",
+    sitename="LlamaCpp.jl",
+    format=Documenter.HTML(;
+        canonical="https://marcom.github.io/LlamaCpp.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+        "API Reference" => "api.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/marcom/LlamaCpp.jl",
+    devbranch="main",
+)
